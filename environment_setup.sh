8c2a356548d3d7daad3564ce981dbd9f41320752
rauskuma <47134908+rauskuma@users.noreply.github.com>
2019-10-14 15:35:47 +0530

    1st commit

---
+++ environment_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ ! -f /root/.ssh/mykey.pem ] ;
+   echo "Copy the SSH Public Key which is used in AWS instance provisioning as mykey.pem in /root/.ssh of this machine"
+   exit 1
+fi
+
+TOWER_GUID=
+OSP_GUID=
+OPENTLC_LOGIN=prasanna.holla-in.ibm.com
+OPENTLC_PASSWORD=
+GITHUB_REPO=https://github.com/prasanna-holla/ansible_advance_homework
+JQ_REPO_BASE=http://www.opentlc.com/download/ansible_bootcamp
+REGION=us-east-1
+RH_MAIL_ID=prasanna.holla@in.ibm.com
+
+ansible-playbook main_playbook.yml --private-key=/root/.ssh/openstack.pem -u ${OPENTLC_LOGIN} -e tower_GUID=${TOWER_GUID} -e osp_GUID=${OSP_GUID} -e opentlc_login=${OPENTLC_LOGIN} -e path_to_opentlc_key=/root/.ssh/openstack.pem -e param_repo_base=${JQ_REPO_BASE} -e opentlc_password=${OPENTLC_PASSWORD} -e REGION_NAME=${REGION} -e EMAIL=${RH_MAIL_ID} -e github_repo=${GITHUB_REPO}
