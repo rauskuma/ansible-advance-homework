b3c8f9373bc7857778719f1a7e73fde85c8a12ff
rauskuma <47134908+rauskuma@users.noreply.github.com>
2019-10-14 16:55:03 +0530

    updated environment details

--- environment_setup.sh
+++ environment_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -f /root/.ssh/mykey.pem ] ;
+if [ ! -f /root/.ssh/mykey.pem ] ; then
    echo "Copy the SSH Public Key which is used in AWS instance provisioning as mykey.pem in /root/.ssh of this machine"
    exit 1
 fi
@@ -14,4 +14,4 @@ JQ_REPO_BASE=http://www.opentlc.com/download/ansible_bootcamp
 REGION=us-east-1
 RH_MAIL_ID=prasanna.holla@in.ibm.com
 
-ansible-playbook main_playbook.yml --private-key=/root/.ssh/openstack.pem -u ${OPENTLC_LOGIN} -e tower_GUID=${TOWER_GUID} -e osp_GUID=${OSP_GUID} -e opentlc_login=${OPENTLC_LOGIN} -e path_to_opentlc_key=/root/.ssh/openstack.pem -e param_repo_base=${JQ_REPO_BASE} -e opentlc_password=${OPENTLC_PASSWORD} -e REGION_NAME=${REGION} -e EMAIL=${RH_MAIL_ID} -e github_repo=${GITHUB_REPO}
+ansible-playbook main_playbook.yml --private-key=/root/.ssh/mykey.pem -u ${OPENTLC_LOGIN} -e tower_GUID=${TOWER_GUID} -e osp_GUID=${OSP_GUID} -e opentlc_login=${OPENTLC_LOGIN} -e path_to_opentlc_key=/root/.ssh/openstack.pem -e param_repo_base=${JQ_REPO_BASE} -e opentlc_password=${OPENTLC_PASSWORD} -e REGION_NAME=${REGION} -e EMAIL=${RH_MAIL_ID} -e github_repo=${GITHUB_REPO}
