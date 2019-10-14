ca81347b311a7f0d0248cdbd84287d1fe36cb314
rauskuma <47134908+rauskuma@users.noreply.github.com>
2019-10-14 17:21:55 +0530

    commit

--- environment_setup.sh
+++ environment_setup.sh
@@ -7,11 +7,11 @@ fi
 
 TOWER_GUID=
 OSP_GUID=
-OPENTLC_LOGIN=prasanna.holla-in.ibm.com
+OPENTLC_LOGIN=rauskuma-in.ibm.com
 OPENTLC_PASSWORD=
-GITHUB_REPO=https://github.com/prasanna-holla/ansible_advance_homework
+GITHUB_REPO=https://github.com/rauskuma/ansible-advance-homework
 JQ_REPO_BASE=http://www.opentlc.com/download/ansible_bootcamp
 REGION=us-east-1
-RH_MAIL_ID=prasanna.holla@in.ibm.com
+RH_MAIL_ID=rauskuma@in.ibm.com
 
 ansible-playbook main_playbook.yml --private-key=/root/.ssh/mykey.pem -u ${OPENTLC_LOGIN} -e tower_GUID=${TOWER_GUID} -e osp_GUID=${OSP_GUID} -e opentlc_login=${OPENTLC_LOGIN} -e path_to_opentlc_key=/root/.ssh/openstack.pem -e param_repo_base=${JQ_REPO_BASE} -e opentlc_password=${OPENTLC_PASSWORD} -e REGION_NAME=${REGION} -e EMAIL=${RH_MAIL_ID} -e github_repo=${GITHUB_REPO}
